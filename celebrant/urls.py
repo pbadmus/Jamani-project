83583ac6ed4d37988e9607e57400ccf2c3c7006c
Patrick Badmus <badmuspatrick5@gmail.com>
2025-06-02 12:46:10 +0100

    Updated the Url

--- celebrant/urls.py
+++ celebrant/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = 'birthday_app'
 
 urlpatterns = [
-     path('', lambda request: HttpResponse("🎉 Jamani is Live!")),
-    #path('', views.home_page, name='home'),
+     
+    path('', views.home_page, name='home'),
     path('upload-video/', views.upload_video, name='upload_video'),
     path('video-gallery/', views.video_gallery, name='video_gallery'),
     path('rsvp/', views.rsvp_view, name='rsvp'),
