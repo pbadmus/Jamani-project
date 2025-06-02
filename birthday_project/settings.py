94aec9d03ee005618c3ac9ff5fe8cf50b61c56ce
Patrick Badmus <badmuspatrick5@gmail.com>
2025-06-02 12:25:16 +0100

    updated the allowed_hosts

--- birthday_project/settings.py
+++ birthday_project/settings.py
@@ -178,8 +178,7 @@ SECRET_KEY = config('SECRET_KEY', default="django-insecure-v9+n@)^#4k4&tqdu_%c7m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.0.1', 'https://jamani-birthday.onrender.com']
 
 # Application definition
 
